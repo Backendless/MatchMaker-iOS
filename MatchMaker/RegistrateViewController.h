e516551e46048d6eb4691d4e6b6532079da7f992
Yury Yaschenko <yura.yaschenko@gmail.com>
2014-02-04 16:18:04 +0200

    add registration view

---
+++ MatchMaker/RegistrateViewController.h
@@ -0,0 +1,13 @@
+//
+//  RegistrateViewController.h
+//  MatchMaker
+//
+//  Created by Yury Yaschenko on 2/4/14.
+//  Copyright (c) 2014 Backendless. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RegistrateViewController : UIViewController
+
+@end
---
+++ MatchMaker/RegistrateViewController.m
@@ -0,0 +1,39 @@
+//
+//  RegistrateViewController.m
+//  MatchMaker
+//
+//  Created by Yury Yaschenko on 2/4/14.
+//  Copyright (c) 2014 Backendless. All rights reserved.
+//
+
+#import "RegistrateViewController.h"
+
+@interface RegistrateViewController ()
+
+@end
+
+@implementation RegistrateViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    [[self navigationController] setNavigationBarHidden:NO];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
